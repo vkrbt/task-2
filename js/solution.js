885d180822b829133f45d420541fb8e648fd3267
Vladislav Korbut <v.korbut8@gmail.com>
2018-01-18 17:35:21 +0300

    Add island task solution

--- js/solution.js
+++ js/solution.js
@@ -10,8 +10,81 @@
      * @returns {number} кол-во островов
      */
     function solution(map) {
-        // todo: подсчитать кол-во островов на карте
-        return 0;
+        var col;
+        var row;
+        var cols = map.length;
+        var rows = map[0].length;
+        var visited = generateVisitedMap(map);
+        var islands = 0;
+        for (col = 0; col < cols; ++col) {
+            for (row = 0; row < rows; ++row) {
+                if (map[col][row] === 1 && !visited[col][row]) {
+                    visitIsland(map, visited, col, row);
+                    ++islands;
+                }
+            }
+        }
+        return islands;
+    }
+
+    /**
+     *
+     * @param {number[][]} map
+     * @returns {number[][]}
+     */
+    function generateVisitedMap(map) {
+        return map.map(function (col) {
+            return col.map(function (row) {
+                return false;
+            })
+        });
+    }
+
+    /**
+     *
+     * @param {number[][]} map
+     * @param {number[][]} visited
+     * @param {number} col
+     * @param {number} row
+     */
+    function visitIsland(map, visited, col, row) {
+        var neighbour;
+        var dfsStack = [{ col, row }];
+        visited[col][row] = true;
+        while (dfsStack.length > 0) {
+            neighbour = getNextUnvisitedNeighbour(map, visited, dfsStack[dfsStack.length - 1]);
+            if (neighbour !== null) {
+                visited[neighbour.col][neighbour.row] = true;
+                dfsStack.push(neighbour);
+            } else {
+                neighbour = dfsStack.pop();
+            }
+        }
+    }
+
+    /**
+     *
+     * @param {number[][]} map
+     * @param {number[][]} visited
+     * @param {Object} current
+     * @param {number} current.col
+     * @param {number} current.row
+     */
+    function getNextUnvisitedNeighbour(map, visited, current) {
+        var { col, row } = current;
+        if ((map[col + 1] && map[col + 1][row]) && (visited[col + 1] && !visited[col + 1][row])) {
+            return { col: col + 1, row }
+        }
+        if ((map[col - 1] && map[col - 1][row]) && (visited[col - 1] && !visited[col - 1][row])) {
+            return { col: col - 1, row }
+        }
+        if (map[col][row + 1] && !visited[col][row + 1]) {
+            return { col, row: row + 1 }
+        }
+        if (map[col][row - 1] && !visited[col][row - 1]) {
+            return { col, row: row - 1 }
+        }
+        return null;
     }
 
     root.SHRI_ISLANDS.solution = solution;
